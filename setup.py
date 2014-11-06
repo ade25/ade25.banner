af8922993f428927ea3c3c2613e04a8b658a22d7
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-11-06 14:11:49 +0100

    Cleanup code - flake8

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ setup(name='ade25.banner',
       version=version,
       description="Ade25 Banner management",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='',
       author='Vorwaerts Werbung GbR',
       author_email='cb@vorwaerts-werbung.de',
@@ -36,10 +36,4 @@ setup(name='ade25.banner',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      # The next two lines may be deleted after you no longer need
-      # addcontent support from paster and before you distribute
-      # your package.
-      setup_requires=["PasteScript"],
-      paster_plugins = ["ZopeSkel"],
-
       )
