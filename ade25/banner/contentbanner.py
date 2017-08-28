93c51f0df96a0625824eb64348cef1b11f93ef67
Christoph Böhner-Figas <boehner@team23.de>
2017-08-28 17:35:05 +0200

    Add link support to banner elements

--- ade25/banner/contentbanner.py
+++ ade25/banner/contentbanner.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
 """Module providing content banners"""
-
 from Acquisition import aq_inner
 from Acquisition import aq_parent
 from five import grok
 from plone import api
-from zope.component import getMultiAdapter
-
-from zope import schema
-from plone.dexterity.content import Item
-
-from plone.directives import form
+from plone.app.layout.navigation.interfaces import INavigationRoot
 from plone.app.textfield import RichText
+from plone.app.z3cform.widget import LinkFieldWidget
+from plone.dexterity.content import Item
+from plone.directives import form
 from plone.namedfile.field import NamedBlobImage
 from plone.namedfile.interfaces import IImageScaleTraversable
-
-from Products.Five.utilities.marker import mark
-
 from plone.uuid.interfaces import IUUID
-from plone.app.layout.navigation.interfaces import INavigationRoot
+from Products.Five.utilities.marker import mark
+from zope import schema
+from zope.component import getMultiAdapter
 
 from ade25.banner.interfaces import IBannerEnabled
 
@@ -48,6 +44,13 @@ class IContentBanner(form.Schema, IImageScaleTraversable):
     )
     text = RichText(
         title=_(u"Block Body Text"),
+        required=False,
+    )
+    form.widget(link=LinkFieldWidget)
+    link = schema.TextLine(
+        title=_(u"Link"),
+        description=_(u"Optional internal or external link that will be "
+                      u"applied to the banner automatically"),
         required=False,
     )
     image = NamedBlobImage(
